01e43bb714759ca05a6f074f6357d9c712bb4fb4
Joseph Waine <joe.waine@gmail.com>
2013-06-27 03:11:40 -0400

    jquery

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+$(document).ready(function(){
+
 $("#slideshow > div:gt(0)").hide();
 
 setInterval(function() {
@@ -7,4 +9,41 @@ setInterval(function() {
     .fadeIn(1000)
     .end()
     .appendTo('#slideshow');
-},  3000);
+},  5000);
+
+$(".readmore").click(function() {
+    //need the following to toggle
+    // $("#discover").css("visibility", "visible");
+    $(this).hide();
+    $(".addtltxt").slideDown('slow');
+    $(".revert").show();
+  });
+
+$(".readmore2").click(function() {
+    //need the following to toggle
+    // $("#discover").css("visibility", "visible");
+    $(this).hide();
+    $(".addtltxt2").slideDown('slow');
+    $(".revert2").show();
+});
+
+$(".revert").click(function() {
+    //need the following to toggle
+    // $("#discover").css("visibility", "visible");
+    $(this).hide();
+    $(".addtltxt").slideUp('slow');
+    $(".readmore").show();
+  });
+
+$(".revert2").click(function() {
+    //need the following to toggle
+    // $("#discover").css("visibility", "visible");
+    $(this).hide();
+    $(".addtltxt2").slideUp('slow');
+    $(".readmore2").show();
+  });
+
+
+
+
+});
